232d97fb8065498f37fa6d62246a9acb672489a8
josuelandero13 <josue.landero130899junior>
2023-11-23 16:11:47 -0600

    - The controller and its logic corresponding to the products are generated - Added index and show views for products - Creation of tests for the product controller. - App testing - Routes for products

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  get "/products", to: "products#index"
+  get "/products/:id", to: "products#show", as: :product
 end
