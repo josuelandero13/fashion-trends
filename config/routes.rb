d2c33b465c16620f6693d4b0339b398b9d0d1045
josuelandero13 <josue.landero130899junior>
2023-11-23 17:19:39 -0600

    - Added route for the new action - View was created for the new action - A form is added to add products to the app - Image was deleted - Test of the new form was carried out

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-
+  
+  get "/products/new", to: "products#new", as: :new_product
   get "/products", to: "products#index"
   get "/products/:id", to: "products#show", as: :product
 end
